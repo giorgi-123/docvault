112246cda066fbe7de15eff39a4d1aa7180e487f
George-203 <gnebieridze@proxima.solutions>
2026-02-24 17:37:13 +0400

    [FIX] added router instead of fastapi app

---
+++ src/docvault/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+
+from docvault.routers.auth import router
+
+
+app = FastAPI(
+    title="Document Vault",
+    description="A place where you can save your documents using API endpoints",
+    version="0.1.0"
+)
+
+@app.get("/health")
+def health_check():
+    """
+        Health check endpoint
+    """
+    return {"status": "healthy"}
+
+
+app.include_router(router)
--- src/docvault/routers/auth.py
+++ src/docvault/routers/auth.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends, HTTPException
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from docvault.schemas.user import UserCreate, UserLogin, UserResponse, Token
+from docvault.services.user import create_user, get_user_by_email
+from docvault.database import get_db
 
+# The prefix part in router adds `/auth` at the beginning of the all routes created using this `router(APIRouter)`
+# So when we register new endpoints in code for example will be `/register` but in use it will be `/auth/register`
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
 
-app = FastAPI(
-    title="Doc Vault",
-    description="Document vault",
-    version="0.1.0")
-
-
-@app.post("/auth/register", response_model=UserResponse)
-def register(data: UserCreate):
+@router.post("/register", response_model=UserResponse)
+async def register(data: UserCreate, session: AsyncSession = Depends(get_db)):
     pass
 
 
-@app.post("/auth/login", response_model=Token)
-def login(data: UserLogin):
+@router.post("/login", response_model=Token)
+async def login(data: UserLogin):
     pass
