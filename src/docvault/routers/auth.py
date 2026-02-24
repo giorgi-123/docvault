2d54d244e3e4cb35e6613d163a9543b2c4a0b576
0xid <giorginebieridze203@gmail.com>
2026-02-25 01:13:41 +0400

    [ADD] finished registration and login endpoints

--- src/docvault/routers/auth.py
+++ src/docvault/routers/auth.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
-
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from datetime import datetime
 
 from docvault.schemas.user import UserCreate, UserLogin, UserResponse, Token
 from docvault.services.user import create_user, get_user_by_email
+from docvault.services.auth import auth_service
 from docvault.database import get_db
+
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 # The prefix part in router adds `/auth` at the beginning of the all routes created using this `router(APIRouter)`
@@ -17,11 +20,30 @@ router = APIRouter(
 )
 
 @router.post("/register", response_model=UserResponse)
-async def register(data: UserCreate, session: AsyncSession = Depends(get_db)):
-    print(data)
-    return UserResponse(id=1, email=data.email, full_name=data.full_name, created_at=datetime.now())
+async def register(data: UserCreate, session: AsyncSession=Depends(get_db)):
+    get_user = await get_user_by_email(session, data.email)
+    if get_user:
+        raise HTTPException(status_code=400, detail="Email already registered!")
+    user = await create_user(session, data)
+    return UserResponse(
+        id=user.id,
+        email=user.email,
+        full_name=user.full_name,
+        created_at=user.created_at
+    )
 
 
 @router.post("/login", response_model=Token)
-async def login(data: UserLogin):
-    pass
+async def login(data: UserLogin, session: AsyncSession=Depends(get_db)):
+    user = await get_user_by_email(session, data.email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid credentials!")
+    valid_password = auth_service.verify_password(data.password, user.password_hash)
+    if not valid_password:
+        raise HTTPException(status_code=401, detail="Invalid credentials!")
+    token = auth_service.create_access_token({"sub": data.email})
+    token = Token(
+        access_token=token,
+    )
+    return token
+
