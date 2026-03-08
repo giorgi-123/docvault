a114f204d305c802d5f5a95f49af28c68bc466dc
0xid <giorginebieridze203@gmail.com>
2026-03-08 13:47:48 +0400

    [ADD] logic for folder model endpoints

--- src/docvault/routers/folder.py
+++ src/docvault/routers/folder.py
@@ -1,4 +1,4 @@
-from fastapi.routing import APIRoute, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from docvault.schemas.folder import FolderCreate, FolderResponse, FolderList
@@ -7,39 +7,100 @@ from docvault.services.auth import get_current_user
 from docvault.services.folder import create_folder, delete_folder, get_folder_by_id, get_user_folders
 from docvault.models.user import User
 
-
-folder_router = APIRoute(
+folder_router = APIRouter(
     prefix="/folder",
     tags=["folder"]
 )
 
+def raise_unauthorized():
+    raise HTTPException(
+                status_code=401,
+                detail="Unauthorized",
+                headers={"WWWW-Authenticate": "Bearer"})
 
 @folder_router.post("/folders", response_model=FolderResponse)
-async def create_folder(
+async def create_folder_endpoint(
     data: FolderCreate,
     session: AsyncSession=Depends(get_db), 
     current_user: User=Depends(get_current_user)
-):
-    pass
+):  
+    folder = await create_folder(
+        session=session,
+        user_id=current_user.id,
+        folder_data=data,
+    )
+    return FolderResponse(
+        id=folder.id,
+        name=folder.name,
+        parent_id=folder.parent_id,
+        created_at=folder.created_at
+    )
 
 @folder_router.get("/folders", response_model=FolderList)
-async def user_folders(
+async def user_folders_endpoint(
+    parent_id: int = None,
     session: AsyncSession=Depends(get_db),
     current_user: User=Depends(get_current_user)
 ):
-    pass
+    folders = await get_user_folders(
+        session=session,
+        user_id=current_user.id,
+        parent_id=parent_id,
+    )
+    
+    if not folders:
+        raise HTTPException(
+            status_code=404,
+            detail="No folder exists on current user!"
+            )
+
+    return FolderList(
+        folders=folders
+    )
 
 @folder_router.get("/folders/{folder_id}", response_model=FolderResponse)
-async def get_single_folder(
+async def get_single_folder_endpoint(
     folder_id: int,
+    session: AsyncSession=Depends(get_db),
     current_user: User=Depends(get_current_user)
 ):
-    pass
+    folder = await get_folder_by_id(
+        session=session,
+        folder_id=folder_id,
+        user_id=current_user.id
+    )
+
+    if not folder:
+        raise HTTPException(
+            status_code=404,
+            detail="Folder not found!",
+        )
+
+    return FolderResponse(
+        id=folder.id,
+        name=folder.name,
+        parent_id=folder.parent_id,
+        created_at=folder.created_at
+    )
 
 @folder_router.delete("/folders/{folder_id}")
-async def delete_folder(
+async def delete_folder_endpoint(
     folder_id: int,
+    session: AsyncSession=Depends(get_db),
     current_user: User=Depends(get_current_user)
 ):
-    pass
+    folder_delete = await delete_folder(
+        session=session,
+        folder_id=folder_id,
+        user_id=current_user.id
+    )
+    if not folder_delete:
+        raise HTTPException(
+            status_code=404,
+            detail="Resource has not been found",
+        )
+    return Response(
+        Content="Delete Successful",
+        status_code=204
+        )
 
