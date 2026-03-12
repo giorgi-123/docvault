61b5a6acd480d5650b80ce305debbe3f94dd56d2
0xid <giorginebieridze203@gmail.com>
2026-03-12 07:15:32 +0400

    [ADD] create_file logic

--- src/docvault/services/file.py
+++ src/docvault/services/file.py
@@ -4,8 +4,18 @@ from sqlalchemy import select, delete
 from docvault.schemas.file import FileResponse
 from docvault.models.file import File
 
-async def create_file(session: AsyncSession, file_data: FileResponse) -> File:
-    pass
+async def create_file(session: AsyncSession, user_id: int, file_data: FileResponse) -> File:
+    file = File(
+        name=file_data.name,
+        s3_key=file_data.s3_key,
+        file_type=file_data.file_type,
+        file_size=file_data.file_size,
+        user_id=user_id,
+        folder_id=file_data.folder_id,
+    )
+    session.add(file)
+    result = await session.flush()
+    return file
 
 async def get_user_files():
     pass
