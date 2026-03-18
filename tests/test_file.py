89a6f76a4ec8436de55df30ca7921c4976b9aaae
giorgi-123 <giorginebieridze203@gmail.com>
2026-03-18 08:11:31 +0400

    [ADD] parameters in test functions

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,43 +1,43 @@
 import pytest
 
 
-async def test_upload():
+async def test_upload(client, auth_headers):
     """
     TODO: Simple Description
     """
     pass
 
-async def test_upload_file_to_folder():
+async def test_upload_file_to_folder(client, auth_headers):
     """
     TODO: Simple Description
     """
 
-async def test_list_files():
+async def test_list_files(client, auth_headers):
     """
     TODO: Simple Description
     """
 
-async def test_get_file_by_id():
+async def test_get_file_by_id(client, auth_headers):
     """
     TODO: Simple Description
     """
 
-async def test_get_nonexistent_file():
+async def test_get_nonexistent_file(client, auth_headers):
     """
     TODO: Simple Description
     """
 
-async def test_download_file():
+async def test_download_file(client, auth_headers):
     """
     TODO: Simple Description
     """
 
-async def test_delete_file():
+async def test_delete_file(client, auth_headers):
     """
     TODO: Simple Description
     """
 
-async def test_unauthenticated_access():
+async def test_unauthenticated_access(client):
     """
     TODO: Simple Description
     """
