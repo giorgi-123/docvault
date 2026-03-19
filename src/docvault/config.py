dbbc775e20f49f89e3ca69c175fc9b650c7fa63e
giorgi-123 <giorginebieridze203@gmail.com>
2026-03-19 08:27:22 +0400

    [ADD] ci workflow, [CHANGE] default bucket name

--- src/docvault/config.py
+++ src/docvault/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     algorithm: str = "HS256" # we could use HS512 which would be more secure but trade-off is performance
     access_token_expire_minutes: int = 30
     s3_endpoint: str | None = "http://localhost:4566"
-    bucket_name: str | None = "demo-bucket-name"
+    bucket_name: str | None = "demo-bucket"
     aws_region_name: str = "eu-north-1"
     aws_access_key_id: str = "test"
     aws_secret_access_key: str = "test"
